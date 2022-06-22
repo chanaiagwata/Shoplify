cdf41f4d09243da1de1315db43fe1e0ac5e51402
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-06-22 15:02:32 +0300

    feat:registers shopapp

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    '''
+    Function that renders the landing page
+    '''
+    return render(request, 'index.html')
